c6acfd8d4f2d0af2577b4d9019b705eaf1c7743d
iisyos <kosuke1216a@yahoo.co.jp>
2023-06-26 00:50:28 +0900

    ci

--- main.tf
+++ main.tf
@@ -18,18 +18,105 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "random_id" "bucket_id" {
-  count       = 100
-  byte_length = 8
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  count  = length(random_id.bucket_id.*.hex)
-  bucket = "bucket-${random_id.bucket_id[count.index].hex}"
-  acl    = "private"
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow inbound traffic from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c94855ba95c574c8" # Change this to a valid AMI ID
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id              = aws_subnet.main.id
 
   tags = {
-    Name        = "bucket-${random_id.bucket_id[count.index].hex}"
-    Environment = "Test"
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = [aws_subnet.main.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "production"
+    Name        = "test-lb-tf"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
   }
 }
